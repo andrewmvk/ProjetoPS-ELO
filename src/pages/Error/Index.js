a3e4bb17b3ddcba19604f919c5935af9cc56caeb
LuizFhellippe <fhellippe04092001@gmail.com>
2021-10-03 22:34:03 -0300

    Mudança das páginas de login e cadastro de HTML para JS

---
+++ src/pages/Error/Index.js
@@ -0,0 +1,8 @@
+import { Link } from "react-router-dom"
+
+export default function Error(){
+    return (<>
+        <h1>Página não encontrada</h1>
+        <a href="/Login">Login</a>
+    </>)
+}
---
+++ src/pages/Login/Index.js
@@ -0,0 +1,50 @@
+import './Style.css';
+import React, { useState } from 'react';
+export default function Login(){
+    const [formvalue, setFormValue] = useState()
+
+    const handleInputChange = (e) =>{
+        const{name, value} = e.target
+
+        //const data = formvalue[name ] || {}
+
+        setFormValue({...formvalue, [name]: value})
+    }
+
+    const handleSubmit = (e) =>{
+        e.preventDefault()
+        const formData = new FormData(e.target)
+        const data = Object.fromEntries(formData)
+        console.log(e.target.value)
+
+        console.log(data)
+    }
+    console.log(formvalue)
+
+    return(
+        <body>
+            <div id="container">
+                <div id="conteudo">
+                    <form onSubmit = {handleSubmit}>
+                        <div>
+                            <input class="email" type="email" name="email" id="email"
+                            placeholder="Digite seu email" onChange={handleInputChange} 
+                             required autofocus></input>
+                        </div>
+                        <div>
+                            <input class="password" type="password" name="password" id="password"
+                            placeholder="Digite sua senha" onChange={handleInputChange} 
+                             required></input>
+                        </div>
+                        <div>
+                            <button type="submit" class="submit" onclick="confirmation()">Logar</button>
+                        </div>
+                    </form>
+                    Não possui cadastro?
+                    <a href="Cadastro">Clique aqui</a>
+                </div>
+            </div>
+            <div id="rodape"></div>
+        </body>
+    )
+}
