66a62d0c74afc6a8104979438d21d8f75c1dea0d
gabrielmds0 <gabrielmds0306@gmail.com>
2021-10-06 16:24:31 -0300

    Adição da Home Page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from'react';
+import Body from '../../components/Body';
+import './style.css';
+
+class Home extends Component{
+    render(){
+        return(
+            <>
+                <Body />
+            
+
+            </>
+
+        );
+    }
+}
+
+export default Home
